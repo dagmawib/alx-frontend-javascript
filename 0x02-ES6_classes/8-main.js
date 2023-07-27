df5ac29d0221e4fe2ad0c6a84762ff2d3949208d
Dagib <dagmawibelayneh14@gmail.com>
2023-07-28 00:47:44 +0300

    Update 8-main.js

--- 0x02-ES6_classes/8-main.js
+++ 0x02-ES6_classes/8-main.js
@@ -1,5 +1,5 @@
 import HolbertonClass from "./8-hbtn_class.js";
 
-const hc = new HolbertonClass(12, "Mezzanine");
+const hc = new HolbertonClass(12, "Dagmawi");
 console.log(Number(hc));
 console.log(String(hc));
